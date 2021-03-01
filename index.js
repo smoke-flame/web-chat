5cc8ddc71144469f3cb2639fe666df2c56c163db
smoke-flame <sednev.serhii@gmail.com>
2021-03-01 22:23:23 +0200

    finished project

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+server.listen(3000);
+
+app.get('/', function(request, respons) {
+	respons.sendFile(__dirname + '/index.html');
+});
+
+connections = [];
+
+// Функция, которая сработает при подключении к странице
+// Считается как новый пользователь
+io.sockets.on('connection', function(socket) {
+	console.log("Успешное соединение");
+	connections.push(socket);
+
+	// Функция, которая срабатывает при отключении от сервера
+	socket.on('disconnect', function(data) {
+		connections.splice(connections.indexOf(socket), 1);
+		console.log("Отключились");
+	});
+
+	// Функция получающая сообщение от какого-либо пользователя
+	socket.on('send mess', function(data) {
+		// Внутри функции мы передаем событие 'add mess',
+		// которое будет вызвано у всех пользователей и у них добавиться новое сообщение 
+		io.sockets.emit('add mess', {mess: data.mess, name: data.name, className: data.className});
+	});
+
+});
